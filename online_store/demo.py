f6baec419574403c157847f604242c7164fed428
David Akinola <dakinola54@gmail.com>
2024-12-08 12:02:09 +0100

    checkout system passed

--- online_store/demo.py
+++ online_store/demo.py
@@ -5,17 +5,34 @@ class Customer:
         self.cart = cart
 
     def add_to_cart(self, product, quantity):
-        self.cart[product] = quantity
+        products = ['bread', 'sugar', 'garri']
+        if product not in products:
+            print(f'{product} is not in store')
+        else:
+            self.cart[product] = quantity
 
     def remove_from_cart(self, product):
         del self.cart[product]
 
+    def checkout(self):
+        prices = {'bread': 100, 'sugar': 50, 'garri': 200}
+        total = []
 
-customer1 = Customer('Dave', {'Bread': 4, 'Sugar': 3})
+        for item, price in prices.items():
+            for g, q in self.cart.items():
+                if item in g:
+                    total.append(price * q)
+        return f"Total: {sum(total)}"
+        
+
+
+customer1 = Customer('Dave', {})
+
 print(customer1.cart)
 
-try:
-    customer1.remove_from_cart('Sugar')
-except KeyError:
-    print('No key')
+customer1.add_to_cart('bread', 5)
+customer1.add_to_cart('sugar', 5)
+customer1.add_to_cart('garri', 10)
 print(customer1.cart)
+
+print(customer1.checkout())
