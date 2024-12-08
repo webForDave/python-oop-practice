608078d70c347a60af319dfa2d1843f3744882ea
David Akinola <dakinola54@gmail.com>
2024-12-08 18:16:40 +0100

    use class variable

--- vehicle_rental_system/demo.py
+++ vehicle_rental_system/demo.py
@@ -1,4 +1,6 @@
 class Vehicle():
+
+    rented_vehicles = 0
     
     def __init__(self, vehicle_id, model, status):
         self.vehicle_id = vehicle_id
@@ -17,18 +19,18 @@ class Vehicle():
 vehicle1 = Vehicle(1, 'Toyota', 'available')
 vehicle2 = Vehicle(2, 'Honda', 'available')
 vehicle3 = Vehicle(3, 'Camry', 'available')
-vehicle4 = Vehicle(4, 'Benz', 'rented')
+vehicle4 = Vehicle(4, 'Benz', 'available')
 
 vehicle2.rent_vehicle()
+vehicle1.rent_vehicle()
 vehicle4.return_vhicle()
 
 list_of_vehicles = [vehicle1, vehicle2, vehicle3, vehicle4]
 
-rented_vehicles = 0
 
 for vehicle in list_of_vehicles:
     if vehicle.status == 'rented':
-        rented_vehicles += 1
+        Vehicle.rented_vehicles += 1
 
-print(rented_vehicles)
+print(Vehicle.rented_vehicles)
 
