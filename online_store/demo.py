32b387b0cd32c9afcf374c26602b68a88d9d0009
David Akinola <dakinola54@gmail.com>
2024-12-08 11:43:46 +0100

    remove from cart functionality

--- online_store/demo.py
+++ online_store/demo.py
@@ -6,3 +6,16 @@ class Customer:
 
     def add_to_cart(self, product, quantity):
         self.cart[product] = quantity
+
+    def remove_from_cart(self, product):
+        del self.cart[product]
+
+
+customer1 = Customer('Dave', {'Bread': 4, 'Sugar': 3})
+print(customer1.cart)
+
+try:
+    customer1.remove_from_cart('Sugar')
+except KeyError:
+    print('No key')
+print(customer1.cart)
