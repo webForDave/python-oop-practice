2262bae7181326a1c6a3e691efc79f0f082fc88b
David Akinola <dakinola54@gmail.com>
2024-12-08 14:27:33 +0100

    get song duration functionality

---
+++ playlist_manager/demo.py
@@ -0,0 +1,29 @@
+class Song:
+
+    def __init__(self, title, artist, duration):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+
+
+    def get_duration_in_minutes(self):
+        seconds = self.duration
+
+        if seconds % 60 == 0:
+            return seconds // 60
+        
+        seconds_part = seconds % 60
+        minutes = seconds // 60
+        return f"{minutes}:{seconds_part}"
+    
+
+
+def get_inputs():
+    title = input('Song name: ').strip()
+    artist = input('Song Artist: ').strip()
+    duration = int(input('Song Duration (in seconds): ').strip())
+    return title, artist, duration
+
+song1 = Song(*get_inputs())
+
+print(song1.get_duration_in_minutes())
