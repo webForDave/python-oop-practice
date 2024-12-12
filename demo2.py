93a063da8bd65e33f164568f12b4797669b3e730
David Akinola <dakinola54@gmail.com>
2024-12-12 19:13:42 +0100

    polymorphism

---
+++ demo2.py
@@ -0,0 +1,11 @@
+class Shapes:
+    def area(self):
+        return f"Area of a shape"
+    
+class Circle(Shapes):
+    def area(self):
+        return f"Area of a circle"
+    
+class Rectangle(Shapes):
+    def area(self):
+        return f"Area of a rectangle"
