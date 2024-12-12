78a4f496ad0335b975a87f439ab4dee9fba556dc
David Akinola <dakinola54@gmail.com>
2024-12-12 18:56:13 +0100

    encapsulation

--- demo1.py
+++ demo1.py
@@ -1,10 +1,18 @@
 class Car:
 
     # Constructor
-    def __init__(self, brand, model, year):
+    def __init__(self, brand, model, year, engine_status):
         self.brand = brand
         self.model = model
         self.year = year
+        self.__engine_status = engine_status
 
     def start(self):
         return f"{self.brand} {self.model} is starting..."
+    
+    def check_engine(self):
+        if self.__engine_status == 'not checked':
+            self.__engine_status = 'faulty'
+            return f"This engine is {self.__engine_status}"
+        return f"This engine is good to go"
+    
